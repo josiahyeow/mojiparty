b76934279cfc05166bebc67464c651d73694eb5c
Josiah Yeow <josiahyeow.dev@gmail.com>
2022-02-05 16:07:07 +1100

    footer location credit

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,8 +3,12 @@ import styled from 'styled-components'
 
 const StyledFooter = styled.footer`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   padding: 2rem;
+`
+
+const Credits = styled.span`
+  color: #757575;
 `
 
 const Link = styled.a`
@@ -14,19 +18,14 @@ const Link = styled.a`
 export function Footer() {
   return (
     <StyledFooter>
-      <div className="container">
-        <div className="content has-text-centered">
-          <p>
-            <Link
-              href="https://mojiparty.notion.site/Terms-of-Service-3537ad45bcc04ab4be9962a73eec645f"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Terms of service
-            </Link>
-          </p>
-        </div>
-      </div>
+      <Credits>Made with 🎈 in Melbourne AU</Credits>
+      <Link
+        href="https://mojiparty.notion.site/Terms-of-Service-3537ad45bcc04ab4be9962a73eec645f"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Terms of service
+      </Link>
     </StyledFooter>
   )
 }
